c737e94c3d61d2e962b855fd10e465b7ffcbe782
CharlesMengCA <58993776+CharlesMengCA@users.noreply.github.com>
2022-08-13 22:55:24 -0400

    Add files via upload

---
+++ 9z-zip-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source $(pwd)/functions.sh
+clear && echo $0 $@
+
+mkdir ~/out
+mpv_build=$(find ~/mpv-winbuild-cmake/build64 -type d -name mpv-x86_64-*-git*)
+cp -R --preserve=timestamps $mpv_build ~/out
+
+mpv_dev=$(find ~/mpv-winbuild-cmake/build64/ -type d -name mpv-dev-x86_64-*-git-*)
+mpv_build=$(basename $mpv_build)
+cp -R --preserve=timestamps $mpv_dev/* ~/out/$mpv_build
+
+echo ~/out/$mpv_build
+ls -g -o --group-directories-first --time-style=iso ~/out/$mpv_build
+7z a $mpv_build.7z ~/out/$mpv_build -r
+#--color=auto
